79e78d7739fa2c3a4ff92d4665d413881243988d
lovit <soy.lovit@gmail.com>
2017-06-21 16:51:05 +0900

    Commit 1.1 ㄷ 불규칙 활용

--- soynlp/pos/_stem.py
+++ soynlp/pos/_stem.py
@@ -1,8 +1,29 @@
 def conjugate(v, e):
+    # Develop code
+    verbs = {
+        '깨닫', '불', '묻', '눋', '겯', '믿', '묻', '뜯'
+    }
+
+    eomis = {
+        '아', '어나다', '어'
+    }
+
+    def is_verb(w): return w in verbs
+    def is_eomi(w): return w in eomis
+    
+    v_ = decompose(v[-1])
+    e_ = decompose(e[0])
+    
     # https://namu.wiki/w/한국어/불규칙%20활용
     ## 1. 어간이 바뀌는 불규칙 활용
-    # 1.1. ㄷ 불규칙 활용
-    
+    # 1.1. ㄷ 불규칙 활용: 깨닫 + 아 -> 꺠달아
+    if (v_[2] == 'ㄹ' and e_[0] == 'ㅇ'):
+        # 규칙 활용
+        if v == '믿' or v == '묻' or v == '뜯':
+            return (v, e)
+        canonicalv = v[:-1] + compose(v_[0], v_[1], 'ㄷ')
+        if is_verb(canonicalv) and is_eomi(e):
+            return (canonicalv, e)
     
     # 1.2. 르 불규칙 활용
     
