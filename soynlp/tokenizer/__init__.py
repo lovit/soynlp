40eee8533f29dfa0e2e4b1218bf2dc364fc1d221
lovit <soy.lovit@gmail.com>
2017-09-20 18:58:41 +0900

    Commit MaxLRScoreTokenizer

--- soynlp/tokenizer/__init__.py
+++ soynlp/tokenizer/__init__.py
@@ -1,4 +1,5 @@
 from ._tokenizer import LTokenizer
 from ._tokenizer import MaxScoreTokenizer
+from ._tokenizer import MaxLRScoreTokenizer
 from ._tokenizer import RegexTokenizer
 from ._normalizer import normalize
--- soynlp/tokenizer/_tokenizer.py
+++ soynlp/tokenizer/_tokenizer.py
@@ -209,3 +209,223 @@ class MaxScoreTokenizer:
         subtoken = token[b:]
         score = self.scores.get(subtoken, self.ds)
         return [(subtoken, b, len(token), score, len(subtoken))]
+
+class MaxLRScoreTokenizer:
+    def __init__(self, Dl=None, Dr=None,
+                 preference_l=None, preference_r=None,
+                 lrgraph=None, tokenizer_builder=None
+                ):
+
+        # Normalize L-R graph to prob graph
+        def norm(rdict):
+            sum_ = sum(rdict.values())
+            return {r:c/sum_ for r,c in rdict.items()}
+        self.lrgraph = lrgraph if lrgraph else {}
+        self.lrgraph_norm = {l:norm(rdict) for l,rdict in self.lrgraph.items()}
+
+        # Expanding dictionary from lrgraph
+        #self.Dl, self.Dr = tokenizer_builder(self.lrgraph) if tokenizer_builder else LRTokenizerBuilder()(self.lrgraph)
+        self.Dl, self.Dr = tokenizer_builder(self.lrgraph) if tokenizer_builder else ({}, {})
+        
+        # Dictionary type check
+        if not Dl: Dl = {}
+        if not Dr: Dr = {}
+
+        if not type(Dl) == dict:
+            Dl = {l:1.0 for l in Dl}
+        self.Dl.update(Dl)
+        if not type(Dr) == dict:
+            Dr = {r:1.0 for r in Dr}
+        self.Dr.update(Dr)
+
+        # Add preference words into dictionary
+        self.Pl = preference_l if preference_l else {}
+        self.Pr = preference_r if preference_r else {}
+
+        for l in self.Pl:
+            if not (l in self.Dl):
+                self.Dl[l] = 1.0
+        for r in self.Pr:
+            if not (r in self.Dr):
+                self.Dr[r] = 1.0
+
+        self.lmax = max((len(w) for w in self.Dl)) if self.Dl else 0
+        self.rmax = max((len(w) for w in self.Dr)) if self.Dr else 0
+        self.base_tokenizer = MaxScoreTokenizer(scores=self.Dr)
+
+
+    def tokenize(self, sent, debug=False, flatten=True):
+        sent_ = [self._tokenize(t, debug) for t in sent.split() if t]
+        if flatten:
+            sent_ = [word for words in sent_ for word in words]
+        return sent_
+
+    def _tokenize(self, t, debug=False):
+        candidates = self._initialize(t)
+        candidates_ = self._remove_l_subset(candidates)
+        scores = self._score(candidates_)
+        best = self._find_best(scores)
+        post = self._postprocessing(t, best)
+        if not debug:
+            post = [[(p[0], 'L'), (p[1], 'R')] for p in post]
+            post = [w for p in post for w in p if w[0]]
+        return post
+    
+    def _initialize(self, t):
+        candidates = self._initialize_L(t)
+        candidates = self._initialize_LR(t, candidates)
+        return candidates
+
+    def _initialize_L(self, t):
+        n = len(t)
+        candidates = []
+        for b in range(n):
+            for e in range(b+1, min(n, b+self.lmax)+1):
+                l = t[b:e]
+                if not (l in self.Dl):
+                    continue
+                candidates.append([l,     # 0
+                                   b,     # 1                  
+                                   e,     # 2
+                                   e-b    # 3
+                                  ])
+        return candidates
+
+    def _initialize_LR(self, t, candidates):
+        n = len(t)
+        expanded = []
+        for (l, b, e, len_l) in candidates:
+            for len_r in range(min(self.rmax, n-e)+1):
+                if len_l == 1 and len_r == 0:
+                    continue
+                r = t[e:e+len_r]
+                if r and not (r in self.Dr):
+                    continue
+                expanded.append([l,
+                                 r,
+                                 b,
+                                 e,
+                                 e + len_r,
+                                 len_l,
+                                 len_r,
+                                 len_l + len_r,
+                                ])
+        return sorted(expanded, key=lambda x:x[4])
+    
+    def _remove_l_subset(self, candidates,
+                   lscore_max_difference=0.3,
+                   lscore_max_diffratio=0.5):
+    
+        for c in candidates:
+            c.append(self.Dl.get(c[0], 0))
+            c.append(self.Dr.get(c[1], 0))
+        candidates = sorted(candidates, key=lambda x:-x[-2])
+
+        candidates_ = []
+        while candidates:
+            best = candidates.pop(0)
+            b, e, lscore = best[2], best[3], best[-2]
+
+            exist_longer = False
+            for c in candidates:
+                if c[2] > b or c[3] < e or not (c[2] < b or c[3] > e):
+                    continue
+                if ((lscore - c[-2]) < lscore_max_difference) \
+                    or ((lscore+1e-5) / (c[-2]+1e-5) < lscore_max_diffratio):
+                    exist_longer = True
+                    break
+
+            if not exist_longer:
+                candidates_.append(best)
+
+        return candidates_
+
+    def _score(self, candidates, ensurable_score_l=0.5, ensurable_score_lr_diff=0.3):
+        from collections import defaultdict
+        # With checking R is overlapped next L
+        begin_to_words = defaultdict(lambda: [])
+        for c in candidates:
+            begin_to_words[c[2]].append(c)
+        begin_to_words = dict(begin_to_words)
+
+        scored = []
+
+        candidates = sorted(candidates, key=lambda x:(-x[-2], -x[-1], x[2], -x[5]))
+        while candidates:
+            c = candidates.pop(0)
+            l, r, p0, p1, p2, len_l, len_r, len_lr, score_l, score_r = c
+
+            # Check whether R is overlapped next L
+            if len_r:
+                overlappped = False
+                for b in range(p1, p2):
+                    if overlappped:
+                        break
+                    for word in begin_to_words.get(b, []):
+                        score_diff = word[-2] + self.Pl.get(word[0], 0) - score_r
+                        if (ensurable_score_l <= word[-2]) or (score_diff > ensurable_score_lr_diff):
+                            overlappped = True
+                            break
+                if overlappped:
+                    continue
+
+            total_score = (score_l * 2 if not r else score_l + score_r) + self.Pl.get(l, 0) + self.Pr.get(r, 0)
+            c.append(total_score)
+            scored.append(c)
+        return scored
+
+    def _find_best(self, scores):
+        best = []
+        sorted_ = sorted(scores, key=lambda x:-x[-1])
+        while sorted_:
+            best.append(sorted_.pop(0))
+            (b, e) = (best[-1][2], best[-1][4])
+            removals = [i for i, c in enumerate(sorted_) if b < c[4] and e > c[2]] # Overlap
+            for idx in reversed(removals):
+                del sorted_[idx]
+        return sorted(best, key=lambda x:x[2])
+
+    def _postprocessing(self, t, words):
+        n = len(t)
+        adds = []
+        if words and words[0][2] > 0:
+            adds += self._add_first_subword(t, words)
+        if words and words[-1][3] < n:
+            adds += self._add_last_subword(t, words, n)
+        adds += self._add_inter_subwords(t, words)
+        post = [w for w in words] + adds
+        return sorted(post, key=lambda x:x[2])
+
+    def _add_inter_subwords(self, t, words):
+        adds = []        
+        for i, base in enumerate(words[:-1]):
+            if base[4] == words[i+1][2]:
+                continue
+            b = base[4]
+            e = words[i+1][2]
+            subword = t[b:e]
+            adds += self._base_tokenizing_subword(subword, b)
+        return adds
+
+    def _add_last_subword(self, t, words, n):
+        b = words[-1][3]
+        subword = t[b:]
+        return self._base_tokenizing_subword(subword, b)
+
+    def _add_first_subword(self, t, words):    
+        e = words[0][2]
+        subword = t[0:e]
+        return self._base_tokenizing_subword(subword, 0)
+
+    def _base_tokenizing_subword(self, t, b):
+        words = self.base_tokenizer.tokenize(t)
+        words_ = []
+        b_ = 0
+        for w in words:
+            n = len(w)
+            # TODO: 여기를 바꿔야해
+            if w in self.Dr:
+                words_.append(['', w, b+b_, b+b_, b+n, 0, n, n, 0, self.base_tokenizer.scores[w]])
+            else:
+                words_.append([w, '', b+b_, b+n, b+n, n, 0, n, 0, 0])
+        return words_
