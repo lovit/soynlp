07210cf5b5e78a0d54090f4a797446a30fa8bb78
lovit <soy.lovit@gmail.com>
2018-02-09 14:37:08 +0900

    Sort import order

--- soynlp/pos/__init__.py
+++ soynlp/pos/__init__.py
@@ -1,6 +1,6 @@
 from ._dictionary import Dictionary
 from ._evaluator import BaseEvaluator, SimpleEojeolEvaluator, LREvaluator
+from ._template import BaseTemplateMatcher, EojeolTemplateMatcher, LRTemplateMatcher
 from ._tagger import BaseTagger, SimpleTagger
 from ._tagger import BasePostprocessor, UnknowLRPostprocessor
-from ._template import BaseTemplateMatcher, EojeolTemplateMatcher, LRTemplateMatcher
 #from ._lrtagger import LRMaxScoreTagger
