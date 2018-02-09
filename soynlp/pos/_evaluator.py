a404cab2e9fd4f19e4a8314450ccd9f3b745d621
lovit <soy.lovit@gmail.com>
2018-02-09 17:07:26 +0900

    Add preference

--- soynlp/pos/_evaluator.py
+++ soynlp/pos/_evaluator.py
@@ -49,7 +49,13 @@ class SimpleEojeolEvaluator(BaseEvaluator):
         return score
 
 class LREvaluator(BaseEvaluator):
-    def __init__(self, weights=None):
+    def __init__(self, weights=None, preference=None):
+        """
+        Arguments
+        ---------
+        preference: dict[str][str] = float
+            Word preference. dict[tag][word] = preference_score
+        """
         if not weights:
             weights = (
                 ('is_noun_phrase', 0.3),
@@ -61,6 +67,9 @@ class LREvaluator(BaseEvaluator):
                 ('l_is_a_syllable', -0.2),
             )
         self.weights = weights
+        if not preference:
+            preference = {}
+        self.preference = preference
 
     def select_best(self, candidates):
         scored_candidates = [(c, self.evaluate(c)) for c in candidates]
@@ -70,14 +79,19 @@ class LREvaluator(BaseEvaluator):
     def evaluate(self, candidate):
         is_noun_phrase = candidate.l_tag == 'Noun'
         lr_are_known = candidate.l_tag is not None and candidate.r_tag is not None
-        # preference
+        # tag preference
         is_verb = candidate.l_tag == 'Adverb'
         is_adjective = candidate.l_tag == 'Adjective'
         # length
         l_len = candidate.m - candidate.b
         r_len = candidate.e - candidate.m
         l_is_a_syllable = l_len == 1
-        
+        # user-defined preference
+        preference = 0
+        if self.preference:
+            preference += self.preference.get(candidate.l_tag, {}).get(candidate.l, 0)
+            preference += self.preference.get(candidate.r_tag, {}).get(candidate.r, 0)
+
         score = (is_noun_phrase * self.weights[0][1]
                  + lr_are_known * self.weights[1][1]
                  + is_verb * self.weights[2][1]
@@ -85,6 +99,7 @@ class LREvaluator(BaseEvaluator):
                  + l_len * self.weights[4][1]
                  + r_len * self.weights[5][1]
                  + l_is_a_syllable * self.weights[6][1]
+                 + preference
                 )
 
         return score
