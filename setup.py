8dfa226c9563c9a06c19224409fa125ca1f3ef46
lovit <soy.lovit@gmail.com>
2018-02-12 11:07:32 +0900

    Update long descrption

--- setup.py
+++ setup.py
@@ -8,29 +8,16 @@ setup(
    author_email='soy.lovit@gmail.com',
    url='https://github.com/lovit/soynlp',
    description="Unsupervised Korean Natural Language Processing Toolkits",
-   long_description="""It contains unsupervised word extraction, tokenizers and noun extractors. 
-   These algorithms are not depending training corpus but extract patterns from data by theirselves.
+   long_description="""Python library for Korean Natural Language Processing. 
+   
+   It supports followings.
+       1. Unsupervised word extractor
+       2. Word score based tokenizer
+       3. Noun extractor
+       4. Dictionary based part of speech tagger
+       5. Text normalizer
 
-   Current version has follows
-   - Word extraction
-     - Cohesion score
-     - Branching Entropy
-     - Accessor Variety
-   - Tokenizers
-     - RegexTokenizer
-     - LTokenizer
-     - MaxScoreTokenizer
-   - Noun extractor
-     - LRNounExtractor
-     
-
-   Following packages are helpful
-   - krwordrank: Unsupervised Korean word/keyword extractor
-     - https://github.com/lovit/KR-WordRank
-     - pip install krwordrank
-   - soyspacing: Korean spacing error corrector
-     - https://github.com/lovit/soyspacing
-     - pip install soyspacing
+   Details are written at https://github.com/lovit/soynlp
    """,
    install_requires=["numpy>=1.12.1", "psutil>=5.0.1"],
    keywords = ['korean natural language processing'],
