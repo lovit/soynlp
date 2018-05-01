ae41ea93dde241eee42bec4e2043923e5f9d1fad
lovit <soy.lovit@gmail.com>
2018-05-01 22:28:51 +0900

    Version up 0.0.44

--- soynlp/__init__.py
+++ soynlp/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'soynlp'
-__version__ = '0.0.43'
+__version__ = '0.0.44'
 __author__ = 'Lovit'
 __license__ = 'GPL v3'
 __copyright__ = 'Copyright 2017 Lovit'
@@ -16,6 +16,12 @@ from .utils import get_process_memory
 from .utils import sort_by_alphabet
 from .utils import DoublespaceLineCorpus
 
-__all__ = ['hangle', 'normalizer', 'noun', 'pos', 'tokenizer', 
-           'vectorizer', 'word', 'get_available_memory',
-           'get_process_memory', 'sort_by_alphabet', 'DoublespaceLineCorpus']
+
+__all__ = [
+    # modules
+    'hangle', 'normalizer', 'noun', 'pos', 'tokenizer',
+    'vectorizer', 'word',
+    # utils
+    'get_available_memory', 'get_process_memory',
+    'sort_by_alphabet', 'DoublespaceLineCorpus',
+]
