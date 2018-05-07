4fe7b53dcf570c1a39c8ee088fab893dea744cc2
lovit <soy.lovit@gmail.com>
2018-05-07 21:20:24 +0900

    First commit NounLTokenizer

--- soynlp/tokenizer/__init__.py
+++ soynlp/tokenizer/__init__.py
@@ -3,3 +3,4 @@ from ._tokenizer import MaxScoreTokenizer
 from ._tokenizer import MaxLRScoreTokenizer
 from ._tokenizer import RegexTokenizer
 from ._normalizer import normalize
+from ._noun_tokenizer import NounLTokenizer
---
+++ soynlp/tokenizer/_noun_tokenizer.py
@@ -0,0 +1,60 @@
+from ._tokenizer import MaxScoreTokenizer
+
+class NounLTokenizer:
+
+    def __init__(self, nouns):
+        self._nouns  = nouns
+
+    def tokenize(self, sentence, tolerance=0.0, compose_compound=True):
+
+        tokens = [self._max_length_l_tokenize(token)
+            for token in sentence.split() if token]
+
+        # remove eojeols which noun does not exist
+        tokens = [token for token in tokens if token and token[0]]
+
+        # remove r parts
+        tokens = [token[0] for token in tokens]
+
+        if compose_compound:
+            tokens = [''.join(token) for token in tokens]
+        else:
+            tokens = [unit for token in tokens for unit in token]
+
+        return tokens
+
+    def _max_length_l_tokenize(self, token):
+        
+        def nouns_to_larray_and_r(token, nouns_):
+            e = sum((len(noun) for noun in nouns_))
+            return nouns_, token[e:]
+
+        nouns = []
+        n = len(token)
+
+        # string match for generating candidats
+        for b in range(n):
+            for e in range(b, n+1):
+                subword = token[b:e]
+                if subword in self._nouns:
+                    # (word, begin, length)
+                    nouns.append((subword, b, e - b))
+
+        # sort. fisrt order: begin index, second order: length (desc)
+        nouns = sorted(nouns, key=lambda x:(x[1], -x[2]))
+
+        nouns_ = []
+        e = 0
+
+        while nouns:
+            # pop first element
+            noun, b, len_ = nouns.pop(0)
+            # only concatenate nouns
+            if not (b == e):
+                return nouns_to_larray_and_r(token, nouns_)
+            # append noun and update end index
+            nouns_.append(noun)
+            e = b + len_
+            nouns = [noun for noun in nouns if noun[1] >= e]
+
+        return nouns_to_larray_and_r(token, nouns_)
