5fdd44256c4620b9622222eea169c393e77a3a69
lovit <soy.lovit@gmail.com>
2018-07-09 14:46:57 +0900

    Refactory word-context matrix creator

--- soynlp/vectorizer/_word_context.py
+++ soynlp/vectorizer/_word_context.py
@@ -2,35 +2,65 @@ from soynlp.utils import get_process_memory
 from collections import defaultdict
 from scipy.sparse import csr_matrix
 
-def sent_to_word_context_matrix(sents, windows=3, min_tf=10,
+def sent_to_word_contexts_matrix(sents, windows=3, min_tf=10,
         tokenizer=lambda x:x.split(), verbose=True):
+
+    if verbose:
+        print('Create (word, contexts) matrix')
+
+    vocab2idx, idx2vocab = _scanning_vocabulary(
+        sents, min_tf, tokenizer, verbose)
+
+    word2contexts = _word_context(
+        sents, windows, tokenizer, verbose, vocab2idx)
+
+    x = _encode_as_matrix(word2contexts, vocab2idx, verbose)
+
+    if verbose:
+        print('  - done')
+    return x, idx2vocab
+
+def _scanning_vocabulary(sents, min_tf, tokenizer, verbose):
 
     # counting word frequency, first
     word_counter = defaultdict(int)
+
     for i_sent, sent in enumerate(sents):
+
         if verbose and i_sent % 1000 == 0:
-            print('\rcounting word frequency from {} sents, mem={} Gb'.format(
-                i_sent, '%.3f' % get_process_memory()), flush=True, end='')
+            _print_status('  - counting word frequency', i_sent)
+
         words = tokenizer(sent)
         for word in words:
             word_counter[word] += 1
+
     if verbose:
-        print('\rcounting word frequency from {} sents was done. mem={} Gb'.format(
-            i_sent, '%.3f' % get_process_memory()), flush=True, end='')
-    
+        _print_status('  - counting word frequency', i_sent, new_line=True)
+
     # filtering with min_tf    
-    vocabulary = {word for word, count in word_counter.items() if count >= min_tf}
-    vocabulary = {word:idx for idx, word in enumerate(sorted(vocabulary, key=lambda w:-word_counter[w]))}
-    idx2vocab = [word for word, _ in sorted(vocabulary.items(), key=lambda w:w[1])]
+    vocab2idx = {word for word, count in word_counter.items() if count >= min_tf}
+    vocab2idx = {word:idx for idx, word in enumerate(
+        sorted(vocab2idx, key=lambda w:-word_counter[w]))}
+    idx2vocab = [word for word, _ in sorted(vocab2idx.items(), key=lambda w:w[1])]
     del word_counter
 
+    return vocab2idx, idx2vocab
+
+def _print_status(message, i_sent, new_line=False):
+    print('\r{} from {} sents, mem={} Gb'.format(
+            message, i_sent, '%.3f' % get_process_memory()),
+        flush=True, end='\n' if new_line else ''
+    )
+
+def _word_context(sents, windows, tokenizer, verbose, vocab2idx):
+
     # scanning (word, context) pairs
-    base2contexts = defaultdict(lambda: defaultdict(int))
+    word2contexts = defaultdict(lambda: defaultdict(int))
 
     for i_sent, sent in enumerate(sents):
+
         if verbose and i_sent % 1000 == 0:
-            print('\rscanning (word, context) pairs from {} sents, mem={} Gb'.format(
-                i_sent, '%.3f' % get_process_memory()), flush=True, end='')
+            _print_status('  - scanning (word, context) pairs', i_sent)
 
         words = tokenizer(sent)
         if not words:
@@ -38,41 +68,43 @@ def sent_to_word_context_matrix(sents, windows=3, min_tf=10,
 
         n = len(words)
 
-        for i, base in enumerate(words):
-            if not (base in vocabulary):
+        for i, word in enumerate(words):
+            if not (word in vocab2idx):
                 continue
 
             # left_contexts
             for context in words[max(0, i-windows):i]:
-                if not (context in vocabulary):
+                if not (context in vocab2idx):
                     continue
-                base2contexts[base][context] += 1
+                word2contexts[word][context] += 1
 
             # right_contexts
             for context in words[min(i+1, n):min(i+windows, n)+1]:
-                if not (context in vocabulary):
+                if not (context in vocab2idx):
                     continue
-                base2contexts[base][context] += 1
+                word2contexts[word][context] += 1
 
     if verbose:
-        print('\rscanning (word, context) pairs from {} sents was done. mem={} Gb'.format(
-            i_sent + 1, '%.3f' % get_process_memory()), flush=True, end='')
+        _print_status('  - scanning (word, context) pairs', i_sent, new_line=True)
 
-    # Encoding dict to vectors
+    return word2contexts
+
+def _encode_as_matrix(word2contexts, vocab2idx, verbose):
+
     rows = []
     cols = []
     data = []
-    for base, contexts in base2contexts.items():
-        base_idx = vocabulary[base]
+    for word, contexts in word2contexts.items():
+        word_idx = vocab2idx[word]
         for context, cooccurrence in contexts.items():
-            context_idx = vocabulary[context]
-            rows.append(base_idx)
+            context_idx = vocab2idx[context]
+            rows.append(word_idx)
             cols.append(context_idx)
             data.append(cooccurrence)
     x = csr_matrix((data, (rows, cols)))
 
     if verbose:
-        print('\r(word, context) matrix was constructed. shape = {}{}'.format(
+        print('  - (word, context) matrix was constructed. shape = {}{}'.format(
             x.shape, ' '*20))
 
-    return x, idx2vocab
+    return x
