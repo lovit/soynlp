f3615f278e1212cd165fa1682e0ce23e25c75c5a
lovit <soy.lovit@gmail.com>
2018-07-18 03:06:16 +0900

    Change default parameter

--- soynlp/lemmatizer/_stem.py
+++ soynlp/lemmatizer/_stem.py
@@ -1,8 +1,8 @@
 import math
 
 def extract_domain_stem(prediction_scores, lrgraph, known_stem_L,
-    ignore_L=None, min_eomi_score=0.3, min_eomi_frequency=100,
-    min_L_score=0.3, min_L_frequency=100, min_num_of_unique_firstchar=4,
+    ignore_L=None, min_eomi_score=0.5, min_eomi_frequency=100,
+    min_L_score=0.7, min_L_frequency=100, min_num_of_unique_firstchar=10,
     min_entropy_of_firstchar=0.5, min_stem_entropy=1.5):
 
     R = {r for r, score in prediction_scores.items()
