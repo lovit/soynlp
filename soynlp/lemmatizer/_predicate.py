b228733b930ce6a1dc5e7d4287b99e3b71decc59
lovit <soy.lovit@gmail.com>
2018-07-18 02:02:28 +0900

    Rename

--- soynlp/lemmatizer/_predicate.py
+++ soynlp/lemmatizer/_predicate.py
@@ -272,7 +272,7 @@ class EomiExtractor:
     def extract_domain_stem(self, append_extracted_stem=True,
         eomi_candidates=None, ignore_L=None,
         min_eomi_score=0.3, min_eomi_frequency=100,
-        min_stem_score=0.3, min_stem_frequency=100,
+        min_L_score=0.3, min_L_frequency=100,
         min_num_of_unique_firstchar=4, min_entropy_of_firstchar=0.5,
         min_stem_entropy=1.5):
 
@@ -295,8 +295,8 @@ class EomiExtractor:
             ignore_L,
             min_eomi_score,
             min_eomi_frequency,
-            min_stem_score,
-            min_stem_frequency,
+            min_L_score,
+            min_L_frequency,
             min_num_of_unique_firstchar,
             min_entropy_of_firstchar,
             min_stem_entropy
