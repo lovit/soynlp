c1f43bd5db35a8f8966c1718d3aae68105ae96a2
lovit <soy.lovit@gmail.com>
2018-08-02 01:59:14 +0900

    Commit extract func

--- soynlp/lemmatizer/_eomi.py
+++ soynlp/lemmatizer/_eomi.py
@@ -1,10 +1,20 @@
+from collections import namedtuple
+
+EomiScore = namedtuple('EomiScore', 'frequency score')
+
 class EomiExtractor:
 
-    def __init__(self, lrgraph, stem_surface, nouns, verbose=True):
+    def __init__(self, lrgraph, stem_surface, nouns, verbose=True, logpath=None):
         self.lrgraph = lrgraph
         self._stem_surface = stem_surface
         self._nouns = nouns
         self.verbose = verbose
+        self.logpath = logpath
+        self._eomis = None
+
+    @property
+    def is_trained(self):
+        return not self._eomis
 
     def _print(self, message, replace=False, newline=True):
         header = '[Eomi Extractor]'
@@ -14,6 +24,46 @@ class EomiExtractor:
         else:
             print('{} {}'.format(header, message),
                   end='\n' if newline else '', flush=True)
+
+    def extract(self, condition=None, minimum_score=0.3,
+        min_num_of_features=5, min_count=1, reset_lrgraph=True):
+
+        # reset covered eojeol count and extracted eomis
+        self._num_of_covered_eojeols = 0
+        self._eomis = {}
+
+        # base prediction
+        candidates = self._candidates_from_stem_surfaces(condition)
+
+        prediction_scores = self._batch_prediction(
+            candidates, minimum_score, min_num_of_features)
+
+        eomis = {eomi:score for eomi, score in prediction_scores.items()
+            if (score[0] >= minimum_score) and (score[1] >= min_count)}
+
+        if self.logpath:
+            with open(self.logpath+'_prediction_score.log', 'w', encoding='utf-8') as f:
+                f.write('eomi score frequency\n')
+
+                for word, score in sorted(prediction_scores.items(), key=lambda x:-x[1][1]):
+                    f.write('{} {} {}\n'.format(word, score[0], score[1]))
+
+        eomis = self._post_processing(eomis, prediction_scores)
+
+        if self.verbose:
+            message = '{} eomis extracted with min count = {}, min score = {}'.format(
+                len(eomis), min_count, minimum_score)
+            self._print(message, replace=False, newline=True)
+
+        self._check_covered_eojeols(eomis)
+
+        self._eomis = eomis
+
+        if reset_lrgraph:
+            self.lrgraph.reset_lrgraph()
+
+        eomis_ = {eomi:EomiScore(score[1], score[0]) for eomi, score in eomis.items()}
+        return eomis_
 
     def predict(self, r, minimum_score=0.3,
         min_num_of_features=5, debug=False):
@@ -76,7 +126,7 @@ class EomiExtractor:
         return [(l, count) for l, count in features if
             ((l in self._stem_surface) and (not self._exist_longer_pos(l, r)))]
 
-    def _eomi_candidates_from_stems(self, condition=None):
+    def _candidates_from_stem_surfaces(self, condition=None):
 
         def satisfy(word, e):
             return word[-e:] == condition
@@ -99,7 +149,7 @@ class EomiExtractor:
 
         return R_from_L
 
-    def _batch_predicting_eomis(self, eomi_candidates,
+    def _batch_prediction(self, eomi_candidates,
         minimum_score=0.3, min_num_of_features=5):
 
         prediction_scores = {}
@@ -130,3 +180,14 @@ class EomiExtractor:
             self._print(message, replace=True, newline=True)
 
         return prediction_scores
+
+    def _post_processing(self, eomis, prediction_scores):
+        # TODO
+        # Remove E + V + E : -서가지고
+
+        # Remove V + E : -싶구나
+        return eomis
+
+    def _check_covered_eojeols(self, eomis):
+        # TODO
+        return None
