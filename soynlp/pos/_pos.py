c345d6b38dcfa57decc199db5e2076ec6ad049de
lovit <soy.lovit@gmail.com>
2018-11-07 23:27:24 +0900

    Re-implement pos extractor postprocessing (dictionary-template matching)

--- soynlp/pos/_pos.py
+++ soynlp/pos/_pos.py
@@ -1,5 +1,9 @@
+from collections import defaultdict
+
 from soynlp.noun import LRNounExtractor_v2
 from soynlp.predicator import PredicatorExtractor
+from soynlp.lemmatizer import _lemma_candidate
+from soynlp.lemmatizer import conjugate
 from soynlp.utils import LRGraph
 
 
@@ -51,21 +55,24 @@ class POSExtractor:
             min_eomi_frequency, min_num_of_unique_R_char, min_entropy_of_R_char,
             min_entropy_of_R, min_stem_score, min_stem_frequency)
 
-        predicators = set(adjectives.keys())
-        predicators.update(set(verbs.keys()))
-        nouns_, confuseds = self._remove_confused_nouns(nouns, predicators)
+        adjective_stems = self.predicator_extractor._adjective_stems
+        verb_stems = self.predicator_extractor._verb_stems
+        eomis = self.predicator_extractor._eomis
+
+        nouns_, confused_nouns, adjectives_, verbs_ = self._word_match_postprocessing(
+            nouns, adjectives, adjective_stems, verbs, verb_stems, eomis)
 
         wordtags = {
             'Noun': nouns_,
-            'Eomi': self.predicator_extractor._eomis,
-            'Adjective': adjectives,
-            'AdjectiveStem': self.predicator_extractor._adjective_stems,
-            'Verb': verbs,
-            'VerbStem': self.predicator_extractor._verb_stems
+            'Eomi': eomis,
+            'Adjective': adjectives_,
+            'AdjectiveStem': adjective_stems,
+            'Verb': verbs_,
+            'VerbStem': verb_stems
         }
 
         if debug:
-            wordtags['ConfusedNoun'] = confuseds
+            wordtags['ConfusedNoun'] = confused_nouns
 
         return wordtags
 
@@ -137,40 +144,131 @@ class POSExtractor:
 
         return adjectives, verbs
 
-    def _remove_confused_nouns(self, nouns, predicators):
-        nouns_ = {}
-        removals = {}
+    def _word_match_postprocessing(self, nouns, adjectives,
+        adjective_stems, verbs, verb_stems, eomis):
 
-        for noun, score in nouns.items():
-            if self._is_noun_predicator_compound(noun, nouns, predicators):
-                removals[noun] = score
-            else:
-                nouns_[noun] = score
-        return nouns_, removals
+        def as_percent(value, total):
+            return 100 * value / total
 
-    def _is_noun_predicator_compound(self, noun, nouns, predicators):
-        def is_noun_josa(prefix):
-            if prefix in nouns:
-                return True
-            for i in range(2, len(prefix)):
-                l, r = prefix[:i], prefix[i:]
-                if (l in nouns) and is_pos_feature(r):
-                    return True
-            return False
+        eojeol_counter = self.noun_extractor.lrgraph.to_EojeolCounter(
+            reset_lrgraph=True)
 
-        def is_pos_feature(r):
-            return ((r in predicators) or
-                    (r in self.noun_extractor._pos_features) or
-                    (r in self.noun_extractor._common_features))
+        stems = set(adjective_stems)
+        stems.update(set(verb_stems))
 
-        if noun in predicators:
-            return True
+        total_frequency = 0
+        nouns_ = defaultdict(int)
+        confused_nouns = defaultdict(int)
+        adjectives_ = {}
+        verbs_ = {}
 
-        n = len(noun)
-        for i in range(-1, -n + 1, -1):
-            prefix, suffix = noun[:i], noun[i:]
-            if (prefix in predicators) and (suffix in predicators):
-                return True
-            if is_noun_josa(prefix) and (suffix in predicators):
-                return True
-        return False
+        for i, (eojeol, count) in enumerate(eojeol_counter.items()):
+
+            if self._verbose and i % 1000 == 0:
+                print('\r[POS Extractor] postprocessing {:.3f} % ...'.format(
+                    as_percent(i, len(eojeol_counter))), end='')
+
+            # cumulate total frequency
+            total_frequency += count
+
+            # check whether eojeol is predicator or not
+            is_predicator = False
+            if self._word_match(eojeol, adjectives):
+                adjectives_[eojeol] = count
+                is_predicator = True
+            if self._word_match(eojeol, verbs):
+                verbs_[eojeol] = count
+                is_predicator = True
+            if is_predicator:
+                continue
+
+            # check eojeol is noun + predicator compound
+            noun = self._separate_predicator_from_noun(
+                eojeol, nouns, adjectives, verbs)
+            if noun is not None:
+                nouns_[noun] += count
+                # debug noun extraction results
+                if eojeol in nouns:
+                    confused_nouns[eojeol] += count
+                continue
+
+            # check eojeol is noun + predicator-suspect compound
+            noun = self._separate_predicator_suspect_from_noun(
+                eojeol, nouns, stems, eomis)
+            if noun is not None:
+                nouns_[noun] += count
+                # debug noun extraction results
+                if eojeol in nouns:
+                    confused_nouns[eojeol] += count
+                continue
+
+            # check eojeol is stem + eomi
+            lemmas = self._conjugatable(eojeol, stems, eomis)
+            if lemmas is not None:
+                stem_adjs = {stem for stem, _ in lemmas if stem in adjective_stems}
+                stem_v = {stem for stem, _ in lemmas if stem in verb_stems}
+                if stem_adjs:
+                    adjectives_[eojeol] = count
+                if stem_v:
+                    verbs_[eojeol] = count
+                if eojeol in nouns:
+                    confused_nouns[eojeol] = count
+                continue
+
+            # else if eojeol is known as noun
+            if eojeol in nouns:
+                nouns_[eojeol] += count
+
+        if self._verbose:
+            print('\r[POS Extractor] postprocessing was done 100.00 %    ')
+            print('[POS Extractor] Noun + [Josa/Predicator]: ({}, {:.3f} %)'.format(
+                len(nouns_), as_percent(sum(nouns_.values()), total_frequency)))
+            print('[POS Extractor] Confused nouns          : ({}, {:.3f} %)'.format(
+                len(confused_nouns), as_percent(sum(confused_nouns.values()), total_frequency)))
+            print('[POS Extractor] Adjective : ({}, {:.3f} %)'.format(
+                len(adjectives_), as_percent(sum(adjectives_.values()), total_frequency)))
+            print('[POS Extractor] Verb      : ({}, {:.3f} %)'.format(
+                len(verbs_), as_percent(sum(verbs_.values()), total_frequency)))
+
+        return nouns_, confused_nouns, adjectives_, verbs_
+
+    def _word_match(self, word, references):
+        if word in references:
+            return word
+        return None
+
+    def _separate_predicator_from_noun(self, word, nouns, adjectives, verbs):
+
+        # Ignore 1-syllable noun
+#             for i in range(len(word), 1, -1):
+        for i in range(2, len(word)):
+            l, r = word[:i], word[i:]
+            if not (l in nouns):
+                continue
+            if (r in adjectives) or (r in verbs):
+                return l
+        return None
+
+    def _separate_predicator_suspect_from_noun(self, word, nouns, stems, eomis):
+        for i in range(2, len(word)):
+            l, r = word[:i], word[i:]
+            if not (l in nouns):
+                continue
+            if self._conjugatable(r, stems, eomis) is not None:
+                return l
+        return None
+
+    def _conjugatable(self, word, stems, eomis):
+        def only_knowns(lemmas):
+            return [lemma for lemma in lemmas if
+                    (lemma[0] in stems) and (lemma[1] in eomis)]
+
+        for i in range(len(word) + 1, 0, -1):
+            try:
+                lemmas = _lemma_candidate(word[:i], word[i:])
+            except:
+                continue
+            lemmas = only_knowns(lemmas)
+            if lemmas:
+                return lemmas
+        return None
