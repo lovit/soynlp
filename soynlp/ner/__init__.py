4207a0eea1d8bbd487f37b73d14de250ef53c039
lovit <soy.lovit@gmail.com>
2018-11-12 17:38:56 +0900

    Commit skeleton of rule based ner

---
+++ soynlp/ner/__init__.py
@@ -0,0 +1,6 @@
+from ._rules import is_date
+from ._rules import is_day
+from ._rules import is_number
+from ._rules import is_monetary
+from ._rules import is_period
+from ._rules import is_time
---
+++ soynlp/ner/_rules.py
@@ -0,0 +1,17 @@
+def is_date(word):
+    raise NotImplemented
+
+def is_day(word):
+    raise NotImplemented
+
+def is_number(word):
+    raise NotImplemented
+
+def is_monetary(word):
+    raise NotImplemented
+
+def is_period(word):
+    raise NotImplemented
+
+def is_time(word):
+    raise NotImplemented
