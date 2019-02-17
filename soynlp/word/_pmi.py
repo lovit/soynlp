c0949de5e23c3f40e9781eede37ebbb51e8096da
lovit <soy.lovit@gmail.com>
2019-02-18 02:20:53 +0900

    Commit fast pmi function

--- soynlp/word/_pmi.py
+++ soynlp/word/_pmi.py
@@ -5,6 +5,70 @@ from sklearn.metrics import pairwise_distances
 from soynlp.utils import get_process_memory
 from soynlp.vectorizer import sent_to_word_contexts_matrix
 
+def _as_diag(px, alpha):
+    px_diag = diags(px.tolist()[0])
+    px_diag.data[0] = np.asarray([0 if v == 0 else 1 / (v + alpha) for v in px_diag.data[0]])
+    return px_diag
+
+def _logarithm_and_ppmi(exp_pmi, min_exp_pmi):
+    # because exp_pmi is sparse matrix and type of exp_pmi.data is numpy.ndarray
+    indices = np.where(exp_pmi.data < min_exp_pmi)[0]
+    exp_pmi.data[indices] = 1
+
+    # apply logarithm
+    exp_pmi.data = np.log(exp_pmi.data)
+    return exp_pmi
+
+def pmi(X, py=None, min_pmi=0, alpha=0.0, beta=1):
+    """
+    :param X: scipy.sparse.csr_matrix
+        (word, contexts) sparse matrix
+    :param py: numpy.ndarray
+        (1, word) shape, probability of context words.
+    :param min_pmi: float
+        Minimum value of pmi. all the values that smaller than min_pmi
+        are reset to zero.
+        Default is zero.
+    :param alpha: float
+        Smoothing factor. pmi(x,y; alpha) = p_xy /(p_x * (p_y + alpha))
+        Default is 0.0
+    :param beta: float
+        Smoothing factor. pmi(x,y) = log ( Pxy / (Px x Py^beta) )
+        Default is 1.0
+    It returns
+    ----------
+    pmi : scipy.sparse.dok_matrix or scipy.sparse.csr_matrix
+        (word, contexts) pmi value sparse matrix
+    px : numpy.ndarray
+        Probability of rows (items)
+    py : numpy.ndarray
+        Probability of columns (features)
+    """
+
+    assert 0 < beta <= 1
+
+    # convert x to probability matrix & marginal probability
+    px = np.asarray((X.sum(axis=1) / X.sum()).reshape(-1))
+    if py is None:
+        py = np.asarray((X.sum(axis=0) / X.sum()).reshape(-1))
+    if beta < 1:
+        py = py ** beta
+        py /= py.sum()
+    pxy = X / X.sum()
+
+    # transform px and py to diagonal matrix
+    # using scipy.sparse.diags
+    # pmi_alpha (x,y) = p(x,y) / ( p(x) x (p(y) + alpha) )
+    px_diag = _as_diag(px, 0)
+    py_diag = _as_diag(py, alpha)
+    exp_pmi = px_diag.dot(pxy).dot(py_diag)
+
+    # PPMI using threshold
+    min_exp_pmi = 1 if min_pmi == 0 else np.exp(min_pmi)
+    pmi = _logarithm_and_ppmi(exp_pmi, min_exp_pmi)
+
+    return pmi, px, py
+
 def pmi_memory_friendly(x, min_pmi=0, alpha=0.0001, verbose=False):
     # convert x to probability matrix & marginal probability 
     px = (x.sum(axis=1) / x.sum()).reshape(-1)
