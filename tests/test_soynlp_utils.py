51776924560ae1f1dab8e29ff0836510b174ca7d
lovit <soy.lovit@gmail.com>
2020-08-15 20:38:54 +0900

    Pytest includes soynlp.utils.EojeolCounter

--- tests/test_soynlp_utils.py
+++ tests/test_soynlp_utils.py
@@ -1,8 +1,8 @@
 import pytest
-from soynlp.utils import LRGraph
+from soynlp.utils import EojeolCounter, LRGraph
 
 
-def test_construct():
+def test_lrgraph_construct():
     sents = ['이것은 예문이다', '이것도 예문이고', '저것은 예시이다']
     lrgraph = LRGraph(sents=sents)
 
@@ -70,7 +70,7 @@ def test_construct():
     }
 
 
-def test_add_pair():
+def test_lrgraph_add_pair():
     lrgraph = LRGraph(l_max_length=3, r_max_length=3)
     lrgraph.add_lr_pair('abc', 'de')
     assert lrgraph._lr == {'abc': {'de': 1}}
@@ -78,7 +78,7 @@ def test_add_pair():
     assert lrgraph._lr == {'abc': {'de': 1}}
 
 
-def test_add_eojeol():
+def test_lrgraph_add_eojeol():
     lrgraph = LRGraph(l_max_length=3, r_max_length=3)
     lrgraph.add_eojeol('abcde')
     assert lrgraph._lr == {'ab': {'cde': 1}, 'abc': {'de': 1}}
@@ -92,7 +92,7 @@ def test_add_eojeol():
     assert lrgraph._lr == {'a': {'bcde': 4}, 'ab': {'cde': 4}, 'abc': {'de': 4}}
 
 
-def test_discount_lr_pair():
+def test_lrgraph_discount_lr_pair():
     lrgraph = LRGraph(l_max_length=3, r_max_length=4)
     lrgraph.add_eojeol('abcde', count=4)
     assert lrgraph._lr == {'a': {'bcde': 4}, 'ab': {'cde': 4}, 'abc': {'de': 4}}
@@ -102,7 +102,7 @@ def test_discount_lr_pair():
     assert lrgraph._rl == {'bcde': {'a': 4}, 'cde': {'ab': 1}, 'de': {'abc': 4}}
 
 
-def test_get_r():
+def test_lrgraph_get_r():
     lrgraph = LRGraph(l_max_length=3, r_max_length=4)
     lrgraph.add_eojeol('이것은', 1)
     lrgraph.add_eojeol('이것도', 2)
@@ -113,7 +113,7 @@ def test_get_r():
     assert lrgraph.get_r('이것', topk=-1) == [('을', 4), ('이', 3), ('도', 2), ('은', 1)]
 
 
-def test_get_l():
+def test_lrgraph_get_l():
     lrgraph = LRGraph(l_max_length=3, r_max_length=4)
     lrgraph.add_eojeol('너의', 1)
     lrgraph.add_eojeol('나의', 2)
@@ -124,10 +124,25 @@ def test_get_l():
     assert lrgraph.get_l('의', topk=-1) == [('시작', 4), ('모두', 3), ('나', 2), ('너', 1)]
 
 
-def test_to_eojeol_counter():
+def test_lrgraph_to_eojeol_counter():
     lrgraph = LRGraph(l_max_length=3, r_max_length=4)
     lrgraph.add_eojeol('너의', 1)
     lrgraph.add_eojeol('나의', 2)
     lrgraph.add_eojeol('모두의', 3)
     lrgraph.add_eojeol('시작의', 4)
     assert sorted(lrgraph.to_EojeolCounter().items(), key=lambda x:x[1]) == [('너의', 1), ('나의', 2), ('모두의', 3), ('시작의', 4)]
+
+
+def test_eojeol_counter():
+    sents = ['이것은 어절 입니다', '이것은 예문 입니다', '이것도 예문 이고요']
+    eojeol_counter = EojeolCounter(sents=sents)
+    assert sorted(eojeol_counter.items()) == [('어절', 1), ('예문', 2), ('이것도', 1), ('이것은', 2), ('이고요', 1), ('입니다', 2)]
+
+    lrgraph = eojeol_counter.to_lrgraph()
+    assert lrgraph.get_r('이것') == [('은', 2), ('도', 1)]
+
+    eojeol_counter.remove_eojeols('이것은')
+    assert sorted(eojeol_counter.items()) == [('어절', 1), ('예문', 2), ('이것도', 1), ('이고요', 1), ('입니다', 2)]
+
+    eojeol_counter.remove_eojeols({'이것도', '입니다'})
+    assert sorted(eojeol_counter.items()) == [('어절', 1), ('예문', 2), ('이고요', 1)]
--- tools/lint.sh
+++ tools/lint.sh
@@ -1,2 +1,2 @@
 flake8 *.py --ignore E501
-flake8 soynlp/ --ignore E501,E741
+flake8 soynlp/ --ignore E501,E741,E128,E125
