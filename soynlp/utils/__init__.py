fb4a7c34fe41e4d55969d141e7e0c166a4e2ec7b
lovit <soy.lovit@gmail.com>
2020-08-16 17:02:05 +0900

    Remove `check_corpus` from init

--- soynlp/utils/__init__.py
+++ soynlp/utils/__init__.py
@@ -2,11 +2,11 @@ from .utils import get_available_memory
 from .utils import get_process_memory
 from .utils import check_dirs
 from .utils import most_similar
-from .utils import check_corpus
 from .utils import DoublespaceLineCorpus
 from .utils import EojeolCounter
 from .utils import LRGraph
 from .math import svd
+
 
 __all__ = [
     # utils
