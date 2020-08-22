7d6ac83128195ee3e54999f3de79c21380caa23f
lovit <soy.lovit@gmail.com>
2020-08-23 02:00:12 +0900

    Complete LRNounExtractor: base_predict

--- tests/test_lrnounextractor.py
+++ tests/test_lrnounextractor.py
@@ -1,9 +1,9 @@
-import pytest
 import os
 import sys
 soynlp_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 sys.path.insert(0, soynlp_path)
-from soynlp.noun.lr import remove_ambiguous_features, _base_predict
+
+from soynlp.noun.lr import remove_ambiguous_features, _base_predict, base_predict
 
 
 def test_base_predict_helper():
@@ -86,7 +86,7 @@ def test_base_predict_helper():
     for test_case in test_cases:
         word = test_case['l']
         features = test_case['features']
-        refined = remove_ambiguous_features(word, features, pos_features, neg_features)
+        refined, _ = remove_ambiguous_features(word, features, pos_features, neg_features)
         pos, common, neg, unk, end = _base_predict(word, refined, pos_features, neg_features, common_features)
         print(f'\nword: {word}\n - before: {features}\n - after:  {refined}')
         print(f' - pos={pos}, neg={neg}, common={common}, end={end}')
@@ -96,3 +96,31 @@ def test_base_predict_helper():
         assert test_case['neg'] == neg
         assert test_case['common'] == common
         assert test_case['end'] == end
+
+
+def test_base_predict():
+    pos_features = {'로', '에', '의', '로는', '에서', '으로', '으로는', '으로써', '이지만'}
+    neg_features = {'다고', '고', '자는', '자고', '지만'}
+    common_features = {'은', '는'}
+
+    test_cases = [
+        {'l': '대학생으', 'r': [('로', 10), ('로써', 5), ('로는', 3)], 'support': 0, 'score': 0},
+        {'l': '대학생', 'r': [('으로', 10), ('으로써', 5), ('으로는', 3)], 'support': 18, 'score': 1.0},
+        {'l': '너로', 'r': [('는', 5), ('', 3)], 'support': 3, 'score': 0},
+        {'l': '너', 'r': [('로는', 5), ('는', 3)], 'support': 8, 'score': 1.0},
+        {'l': '관계자', 'r': [('는', 10), ('로', 5), ('이지만', 5)], 'support': 10, 'score': 1.0},
+        {'l': '관계', 'r': [('자는', 10), ('자로', 5), ('는', 20), ('의', 25), ('자이지만', 5)], 'support': 45, 'score': 0.42857142857142855},
+        {'l': '하자', 'r': [('는', 10), ('고', 5)], 'support': 0, 'score': 0},
+        {'l': '하', 'r': [('자는', 10), ('자고', 5)], 'support': 15, 'score': -1.0},
+        {'l': '가고있다', 'r': [('고', 10), ('지만', 3)], 'support': 3, 'score': 0},
+        {'l': '가고있', 'r': [('다고', 10), ('지만', 5), ('다지만', 3)], 'support': 15, 'score': -1.0},
+        {'l': '경찰국', 'r': [('은', 1), ('에', 1), ('에서', 1)], 'support': 3, 'score': 1.0},
+        {'l': '아이웨딩', 'r': [('', 90), ('은', 3), ('측은', 1)], 'support': 93, 'score': 0.9893617021276596}
+    ]
+    for test_case in test_cases:
+        word = test_case['l']
+        features = test_case['r']
+        support, score = base_predict(word, features, pos_features, neg_features, common_features)
+        print(f'{word} : {features}\n  support={support}, score = {score}\n')
+        assert support == test_case['support']
+        assert score == test_case['score']
