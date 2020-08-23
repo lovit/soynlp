a35ad4acc3c079fc693d34d057d12208d9147cfa
lovit <soy.lovit@gmail.com>
2020-08-23 19:39:57 +0900

    Remove normalizers from tokenizer directory

--- soynlp/tokenizer/__init__.py
+++ soynlp/tokenizer/__init__.py
@@ -2,6 +2,5 @@ from ._tokenizer import LTokenizer
 from ._tokenizer import MaxScoreTokenizer
 from ._tokenizer import MaxLRScoreTokenizer
 from ._tokenizer import RegexTokenizer
-from ._normalizer import normalize
 from ._noun_tokenizer import NounLMatchTokenizer
 from ._noun_tokenizer import NounMatchTokenizer
