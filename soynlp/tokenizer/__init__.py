47bb973e509ceef3b20bb33415cac801fb177863
lovit <soy.lovit@gmail.com>
2020-08-23 23:17:49 +0900

    Remove incompleted class: MaxLRScoreTokenizer

--- soynlp/tokenizer/__init__.py
+++ soynlp/tokenizer/__init__.py
@@ -1,6 +1,5 @@
 from ._tokenizer import LTokenizer
 from ._tokenizer import MaxScoreTokenizer
-from ._tokenizer import MaxLRScoreTokenizer
 from ._tokenizer import RegexTokenizer
 from ._noun_tokenizer import NounLMatchTokenizer
 from ._noun_tokenizer import NounMatchTokenizer
