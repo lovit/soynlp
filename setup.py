cfec859395c27311e2784ac7e91682bef42b5874
lovit <soy.lovit@gmail.com>
2021-01-26 06:51:40 +0900

    Clean-up `setup.py` (#125)

--- setup.py
+++ setup.py
@@ -1,45 +1,57 @@
-import soynlp
-import setuptools
+import os
 from setuptools import setup, find_packages
 
 
-with open('README.md', encoding='utf-8') as fh:
-    long_description = fh.read()
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
-setuptools.setup(
-    name="soynlp",
-    version=soynlp.__version__,
-    author=soynlp.__author__,
-    author_email='soy.lovit@gmail.com',
-    description="Unsupervised Korean Natural Language Processing Toolkits",
+
+def get_about():
+    about = {}
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(basedir, "soynlp", "about.py")) as f:
+        exec(f.read(), about)
+    return about
+
+
+def requirements():
+    requirements_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
+    with open(requirements_path, encoding="utf-8") as f:
+        return f.read().splitlines()
+
+
+about = get_about()
+setup(
+    name=about["__name__"],
+    version=about["__version__"],
+    author=about["__author__"],
+    description=about["__description__"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/lovit/soynlp',
-    packages=setuptools.find_packages(),
+    url="https://github.com/lovit/soynlp",
+    install_requires=requirements(),
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": ["soynlp=soynlp.cli:main"],
+    },
     package_data={
-        'soynlp':[
-            'trained_models/*',
-            'pos/dictionary/*.txt',
-            'pos/dictionary/*/*.txt',
-            'postagger/dictionary/default/*/*.txt',
-            'noun/*.txt',
-            'noun/pretrained_models/*',
-            'lemmatizer/dictionary/default/*/*.txt'
+        "soynlp": [
+            "trained_models/*",
+            "pos/dictionary/*.txt",
+            "pos/dictionary/*/*.txt",
+            "postagger/dictionary/default/*/*.txt",
+            "noun/*.txt",
+            "noun/pretrained_models/*",
+            "lemmatizer/dictionary/default/*/*.txt",
         ]
     },
-    keywords = [
-        'korean-nlp',
-        'korean-text-processing',
-        'nlp',
-        'tokenizer',
-        'postagging',
-        'word-extraction'
-    ],
-    install_requires=[
-        "numpy>=1.12.1",
-        "psutil>=5.0.1",
-        "scipy>=1.1.0",
-        "scikit-learn>=0.20.0",
+    keywords=[
+        "korean-nlp",
+        "korean-text-processing",
+        "nlp",
+        "tokenizer",
+        "postagging",
+        "word-extraction",
     ],
     classifiers=(
         "Programming Language :: Python :: 3.6",
