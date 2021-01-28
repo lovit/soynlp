50b5f7bea3c15318b84fd99ab161cf86e1990a32
lovit <soy.lovit@gmail.com>
2021-01-28 09:21:38 +0900

    단어 추출기: subword counting 까지 재구현 (#115)

--- soynlp/word/word.py
+++ soynlp/word/word.py
@@ -1,5 +1,161 @@
 import math
+from collections import defaultdict
 from dataclasses import dataclass
+from tqdm import tqdm
+
+from soynlp.utils import get_process_memory
+
+
+class WordExtractor:
+    def __init__(self, max_l_length=10, max_r_length=6, verbose=True):
+        self.max_l_length = max_l_length
+        self.max_r_length = max_r_length
+
+        self.L = {}
+        self.R = {}
+        self.prev_L = {}
+        self.R_next = {}
+
+    @property
+    def is_trained(self):
+        return self.L and self.R
+
+    def extract(
+        self,
+        train_data=None,
+        cumulate=True,
+        extract_cohesion_only=False,
+        min_frequency=5,
+        min_cohesion_leftside=0.05,
+        min_cohesion_rightside=0.0,
+        min_brancingentropy_leftside=0.0,
+        min_brancingentropy_rightside=0.0,
+        min_accessorvariety_leftside=1,
+        min_accessorvariety_rightside=1,
+        prune_per_lines=-1,
+        remove_subwords=False,
+    ):
+        L, R, prev_L, R_next = initialize_counters(
+            self.L, self.R, self.prev_L, self.R_next, cumulate
+        )
+        L, R, prev_L, R_next = count_substrings(
+            train_data=train_data,
+            L=L,
+            R=R,
+            prev_L=prev_L,
+            R_next=R_next,
+            max_left_length=self.max_left_length,
+            max_right_length=self.max_right_length,
+            min_frequency=min_frequency,
+            prune_per_lines=prune_per_lines,
+            cohesion_only=cohesion_only,
+            verbose=self.verbose,
+        )
+        self.L, self.R, self.prev_L, self.R_next = L, R, prev_L, R_next
+
+
+def print_message(message):
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[WordExtractor] {now}, mem={get_process_memory():.4} GB : {message}")
+
+
+def initializer_counters(L, R, prev_L, R_next, cumulate: bool):
+    if cumulate:
+        return tuple(defaultdict(int, d) for d in [L, R, prev_L, R_next])
+    return tuple(defaultdict(int) for _ in range(4))
+
+
+def prune_counter(counter, min_count):
+    return defaultdict(
+        int, {key: count for key, count in counter.items() if count >= min_count}
+    )
+
+
+def count_substrings(
+    train_data,
+    L,
+    R,
+    prev_L,
+    R_next,
+    max_left_length,
+    max_right_length,
+    min_frequency,
+    prune_per_lines,
+    cohesion_only,
+    verbose,
+):
+    if not verbose:
+        train_iterator = train_data
+    else:
+        if hasattr(train_data, "__len__"):
+            total = len(train_data)
+        else:
+            total = None
+        desc = "[WordExtractor] counting subwords"
+        train_iterator = tqdm(train_data, desc=desc, total=total)
+
+    for i_line, line in enumerate(train_iterator):
+        # prune
+        if (prune_per_lines > 0) and (i_line % prune_per_lines == 0):
+            L, R, prev_L, R_next = [prune_counter(d, 2) for d in [L, R, prev_L, R_next]]
+
+        words = line.split()
+
+        # cohesion only
+        for word in words:
+            if (not word) or (len(word) <= 1):
+                continue
+            n = len(word)
+            for i in range(1, min(max_left_length, n) + 1):
+                L[word[:i]] += 1
+            for i in range(1, min(max_right_length + 1, n)):
+                R[word[-i:]] += 1
+
+        # branching entropy & accessor variety
+        if (cohesion_only) or (len(words) <= 1):
+            continue
+
+        prev_words = [words[-1]] + words[:-1]
+        next_words = words[1:] + [words[0]]
+        for prev_word, word, next_word in zip(prev_words, words, next_words):
+            prev_char = prev_word[-1]
+            next_char = next_word[0]
+            n = len(word)
+            for i in range(1, min(max_left_length, n) + 1):
+                prev_L[f"{prev_char} {word[:i]}"] += 1
+            for i in range(1, min(max_right_length + 1, n)):
+                R_next[f"{word[-i:]} {next_char}"] += 1
+
+    L = dict(prune_counter(L, min_frequency))
+    R = dict(prune_counter(R, min_frequency))
+    prev_L = dict(prune_counter(prev_L, min_frequency))
+    R_next = dict(prune_counter(R_next, min_frequency))
+    return L, R, prev_L, R_next
+
+
+def get_entropy(collection_of_numbers):
+    """
+    Args:
+        collection_of_numbers (collection of number)
+
+    Returns:
+        entropy (float)
+
+    Examples::
+        >>> get_entropy([3, 4, 3])
+        $ 1.0888999753452238
+
+        >>> get_entropy([100, 1, 1])
+        $ 0.11010008192339721
+    """
+    if not collection_of_numbers:
+        return 0.0
+    total = sum(collection_of_numbers)
+    entropy = 0
+    for number in collection_of_numbers:
+        prob = float(number) / total
+        entropy += prob * math.log(prob)
+    return -1 * entropy
 
 
 @dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=False)
@@ -106,28 +262,3 @@ class AccessorVariety:
     subword: str
     leftside: float
     rightside: float
-
-
-def get_entropy(collection_of_numbers):
-    """
-    Args:
-        collection_of_numbers (collection of number)
-
-    Returns:
-        entropy (float)
-
-    Examples::
-        >>> get_entropy([3, 4, 3])
-        $ 1.0888999753452238
-
-        >>> get_entropy([100, 1, 1])
-        $ 0.11010008192339721
-    """
-    if not collection_of_numbers:
-        return 0.0
-    total = sum(collection_of_numbers)
-    entropy = 0
-    for number in collection_of_numbers:
-        prob = float(number) / total
-        entropy += prob * math.log(prob)
-    return -1 * entropy
--- tests/test_wordextractor.py
+++ tests/test_wordextractor.py
@@ -1,4 +1,11 @@
-from soynlp.word.word import CohesionScore, BranchingEntropy, AccessorVariety, get_entropy
+from collections import defaultdict
+from soynlp.word.word import (
+    CohesionScore,
+    BranchingEntropy,
+    AccessorVariety,
+    get_entropy,
+    count_substrings
+)
 
 
 def test_score_dataclass():
@@ -12,6 +19,89 @@ def test_score_dataclass():
         subword="토크나이저", leftside=0.8, rightside=0.5
     )
 
+
+def test_couting_substrings():
+    train_data = ["여름이는 여름을 좋아한다", "올겨울에는 겨울에 갔다"]
+    L, R, prev_L, R_next = count_substrings(
+        train_data=train_data,
+        L=defaultdict(int),
+        R=defaultdict(int),
+        prev_L=defaultdict(int),
+        R_next=defaultdict(int),
+        max_left_length=3,
+        max_right_length=2,
+        min_frequency=1,
+        prune_per_lines=-1,
+        cohesion_only=False,
+        verbose=True
+    )
+
+    # ["여름이는", "좋아한다", "올겨울에", "올겨울에는"] 는 길이가 4 이상이므로 포함되지 않음
+    L == {
+        '여': 2,
+        '여름': 2,
+        '여름이': 1,
+        '여름을': 1,
+        '좋': 1,
+        '좋아': 1,
+        '좋아한': 1,
+        '올': 1,
+        '올겨': 1,
+        '올겨울': 1,
+        '겨': 1,
+        '겨울': 1,
+        '겨울에': 1,
+        '갔': 1,
+        '갔다': 1
+    }
+
+    # ["갔다"]는 L 이기에 포함되지 않음
+    assert R == {
+        '는': 2,
+        '이는': 1,
+        '을': 1,
+        '름을': 1,
+        '다': 2,
+        '한다': 1,
+        '에는': 1,
+        '에': 1,
+        '울에': 1
+    }
+
+    assert prev_L == {
+        '다 여': 1,
+        '다 여름': 1,
+        '다 여름이': 1,
+        '는 여': 1,
+        '는 여름': 1,
+        '는 여름을': 1,
+        '을 좋': 1,
+        '을 좋아': 1,
+        '을 좋아한': 1,
+        '다 올': 1,
+        '다 올겨': 1,
+        '다 올겨울': 1,
+        '는 겨': 1,
+        '는 겨울': 1,
+        '는 겨울에': 1,
+        '에 갔': 1,
+        '에 갔다': 1
+    }
+
+    # "갔다"는 L 이므로 포함되지 않으며, "갔다 - 올겨울에는" 으로부터 "다 올" 이 계산되었다.
+    assert R_next == {
+        '는 여': 1,
+        '이는 여': 1,
+        '을 좋': 1,
+        '름을 좋': 1,
+        '다 여': 1,
+        '한다 여': 1,
+        '는 겨': 1,
+        '에는 겨': 1,
+        '에 갔': 1,
+        '울에 갔': 1,
+        '다 올': 1
+    }
 
 def test_get_entropy():
     assert abs(get_entropy([3, 4, 3]) - 1.0888999) < 0.0001
