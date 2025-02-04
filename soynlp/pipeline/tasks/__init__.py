ecc89bb2910b79a009b0bca290e8de713bf7f5e3
lovit <soy.lovit@gmail.com>
2025-02-05 04:44:13 +0900

    [#151] feat: Implement WriteTasks

--- soynlp/pipeline/tasks/__init__.py
+++ soynlp/pipeline/tasks/__init__.py
@@ -2,6 +2,8 @@ from soynlp.pipeline.tasks.dummy import DummyTask  # noqa F401
 from soynlp.pipeline.tasks.task import Task  # noqa F401
 from soynlp.pipeline.tasks.read_json import ReadJsonTask  # noqa F401
 from soynlp.pipeline.tasks.read_text import ReadTextTask  # noqa F401
+from soynlp.pipeline.tasks.write_json import WriteJsonTask  # noqa F401
+from soynlp.pipeline.tasks.write_text import WriteTextTask  # noqa F401
 
 
 __all__ = (
@@ -9,4 +11,6 @@ __all__ = (
     "DummyTask",
     "ReadJsonTask",
     "ReadTextTask",
+    "WriteJsonTask",
+    "WriteTextTask",
 )
